81d38f7d5e321bcbe3d673b3af9f6646993e2b58
andrysta <andrysta@andrysta-VirtualBox>
2018-01-16 00:26:24 +0200

    character varying fields update

---
+++ sql/V0010__character_varying_fields_update.sql
@@ -0,0 +1,18 @@
+﻿ALTER TABLE public.categories
+   ALTER COLUMN label TYPE character varying(200);
+ALTER TABLE public.wms_infos
+   ALTER COLUMN title TYPE character varying(200);
+ALTER TABLE public.wms_infos
+   ALTER COLUMN keywords TYPE character varying(200)[];
+ALTER TABLE public.wms_infos
+   ALTER COLUMN formats TYPE character varying(100)[];
+ALTER TABLE public.wmses
+   ALTER COLUMN label TYPE character varying(200);
+ALTER TABLE public.wmses
+   ALTER COLUMN name TYPE character varying(200);
+ALTER TABLE public.wmses
+   ALTER COLUMN url TYPE character varying(500);
+ALTER TABLE public.wmsstyles
+   ALTER COLUMN name TYPE character varying(200);
+ALTER TABLE public.wmsstyles
+   ALTER COLUMN urls TYPE character varying(500)[];
